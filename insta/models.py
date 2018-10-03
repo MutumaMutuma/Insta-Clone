cb4bb4b95e8c148fb1e2751da279805512517a4f
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-03 20:03:34 +0300

    made a profile class

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+# from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=200)
+    profile_pic = models.ImageField(upload_to='profile/')
+    pub_date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.first_name
+        
