c2098c962c85d2a6579b5ae0d96a783da2a9eec7
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-05 17:31:54 +0300

    added new image function

--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 import datetime as dt
+from django.contrib.auth.decorators import login_required
+from .forms import ImageForm
 from django.db import models
 from .models import Image,Profile
 # Create your views here.
@@ -9,10 +11,24 @@ def signup(request):
     date = dt.date.today()
     
     return render(request, 'registration/homepage.html', {"date": date,})
-
+@login_required(login_url='/accounts/login/')
 def index(request):
     date = dt.date.today()
     photos = Image.objects.all()
     print(photos)
     profiles = Profile.objects.all()
     return render(request, 'all-posts/index.html', {"date": date, "photos":photos, "profiles":profiles,})
+
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.editor = current_user
+            image.save()
+        return redirect('NewsToday')
+
+    else:
+        form = ImageForm()
+    return render(request, 'new_image.html', {"form": form})
