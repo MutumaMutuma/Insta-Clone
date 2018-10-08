2ac1fbba46c77a58499e3dd446ca1c5bb1746cdd
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-08 12:13:41 +0300

    added comment form

--- insta/forms.py
+++ insta/forms.py
@@ -1,7 +1,19 @@
 from django import forms
-from .models import Image
+from .models import Image,Profile,Comments
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['likes','profile','posted_time','profile','user']
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['first_name','last_name','bio','profile_pic'] 
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['image','user']
