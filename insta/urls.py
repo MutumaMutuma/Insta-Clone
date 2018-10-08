01af4f16ca5d633c0a71c957c8b535bd6d730ba2
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-08 14:14:47 +0300

    added comment route

--- insta/urls.py
+++ insta/urls.py
@@ -9,7 +9,7 @@ urlpatterns=[
     url(r'^new/image$', views.new_image, name='new-image'),
     url(r'^profile/$',views.profile,name='profile'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^comment/', views.comment, name='comment'),
+    url(r'^comment/(?P<photos_item_id>\d+)', views.comment, name='comment'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
