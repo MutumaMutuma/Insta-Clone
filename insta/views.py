1d49b538e55551773ca8b564366daf7c628fcd04
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-08 12:09:59 +0300

    added a search view

--- insta/views.py
+++ insta/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 import datetime as dt
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from .forms import ImageForm
+from .forms import ImageForm, SignupForm, CommentForm
 from django.db import models
 from .models import Image,Profile
 # Create your views here.
@@ -11,24 +12,80 @@ def signup(request):
     date = dt.date.today()
     
     return render(request, 'registration/homepage.html', {"date": date,})
-@login_required(login_url='/accounts/login/')
+
+
+
+@login_required(login_url='/home')
 def index(request):
     date = dt.date.today()
     photos = Image.objects.all()
-    print(photos)
+    # print(photos)
     profiles = Profile.objects.all()
-    return render(request, 'all-posts/index.html', {"date": date, "photos":photos, "profiles":profiles,})
+    # print(profiles)
+    form = CommentForm()
+
+    return render(request, 'all-posts/index.html', {"date": date, "photos":photos, "profiles":profiles, "form":form})
 
 def new_image(request):
     current_user = request.user
+    profile = Profile.objects.get(user=current_user)
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             image = form.save(commit=False)
             image.user = current_user
+            image.profile = profile
             image.save()
         return redirect('index')
 
     else:
         form = ImageForm()
     return render(request, 'new_image.html', {"form": form})
+
+def profile(request):
+    date = dt.date.today()
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user.id)
+    print(profile.profile_pic)
+    print('esrdcgvybhjn')
+    
+    if request.method == 'POST':
+        signup_form = SignupForm(request.POST, request.FILES,instance=request.user.profile) 
+        if signup_form.is_valid():
+           signup_form.save()
+    else:        
+        signup_form =SignupForm() 
+    
+    return render(request, 'registration/profile.html', {"date": date, "form":signup_form,"profile":profile})
+
+@login_required(login_url='/accounts/login/')
+def comment(request,image_id):
+    #Getting comment form data
+    if request.method == 'POST':
+        image = get_object_or_404(Image, pk = image_id)
+        form = CommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.image = image
+            comment.save()
+            return redirect('index')
+    else:
+        form = CommentForm()
+
+    title = 'Home'
+    return render(request, 'index.html', {'title':title})
+
+def search_results(request):
+
+    if 'username' in request.GET and request.GET["username"]:
+        search_term = request.GET.get("username")
+        searched_usernames = username.search_by_username(search_term)
+        
+        message = f"{search_term}"
+
+        return render(request, 'all-pics/search.html',{"message":message,"usernames": searched_usernames})
+
+    else:
+        message = "You haven't searched for any"
+        return render(request, 'all-pics/search.html',{"message":message})
