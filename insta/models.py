b7f06f46c117df7bee4a228edeaf0c1d15b4ccb9
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-08 12:12:50 +0300

    added comment class

--- insta/models.py
+++ insta/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     bio = models.CharField(max_length=200)
@@ -30,16 +31,22 @@ class Profile(models.Model):
     def search_by_username(cls,search_term):
         profiles = cls.objects.filter(title__icontains=search_term)
         return profiles
-    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
     image_name = models.CharField(max_length=30)
     image_caption = models.CharField(max_length=30)
     image_location = models.CharField(max_length=30,null=True)
-    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    posted_time = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,blank=True, null=True)
+    posted_time = models.DateTimeField(auto_now_add=True,)
     likes = models.PositiveIntegerField(default=0)
 
     class Meta:
@@ -53,4 +60,19 @@ class Image(models.Model):
         images = cls.objects.all()
         return images
 
-# class Likes(models.Model):
+class Comments(models.Model):
+    comment = models.CharField(max_length = 300)
+    posted_on = models.DateTimeField(auto_now=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+    @classmethod
+    def get_comments_by_images(cls, id):
+        comments = Comments.objects.filter(image__pk = id)
+        return comments
