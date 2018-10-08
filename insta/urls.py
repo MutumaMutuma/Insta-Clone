a39de7fc5c384bf83e5a3e9cfd6ac0fad1a6fffc
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-08 12:10:20 +0300

    added a comment url

--- insta/urls.py
+++ insta/urls.py
@@ -7,7 +7,9 @@ urlpatterns=[
     url(r'^$', views.signup, name='signup'),
     url(r'^home/$',views.index,name='index'),
     url(r'^new/image$', views.new_image, name='new-image'),
-    # url(r'^accounts/profile/$',views.profile,name='profile'),
+    url(r'^profile/$',views.profile,name='profile'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^comment/', views.comment, name='comment'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
