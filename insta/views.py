7631c345c543063a3f5ab1205389454b106c91fd
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-09 18:39:45 +0300

    deploying to heroku

--- insta/views.py
+++ insta/views.py
@@ -56,7 +56,7 @@ def activate(request, uidb64, token):
         user.save()
         login(request, user)
         # return redirect('home')
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        return HttpResponse('Thank you for your email confirmation. Now you can <a href="/accounts/login/">Login</a>  your account.')
     else:
         return HttpResponse('Activation link is invalid!')
 
